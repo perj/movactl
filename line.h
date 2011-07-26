88e7b5957a685f51f8452ab60f072ddd5195a681
Per Johansson <per@morth.org>
2011-07-26 23:11:55 +0200

    Prune obsolete (marantz specific) line functions

--- line.h
+++ line.h
@@ -28,8 +28,4 @@
 
 int open_line (const char *path, int mode);
 
-int read_line (int fd, char *buf, int buflen);
-int send_command (int fd, const char *command, const char *arg);
-int send_status_request (int fd, const char *status);
-
 #endif /*LINE_H*/
